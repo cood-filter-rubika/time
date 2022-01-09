1d39fc88eb6a5e5df43b53f3a36d7e7a52ce5c3a
maxtor <96935075+cood-filter-rubika@users.noreply.github.com>
2022-01-09 10:47:38 +0330

    Add files via upload

---
+++ tiem.py
@@ -0,0 +1,54 @@
+import os
+import time
+import requests
+from threading import Thread
+from colorama import Fore
+from time import sleep
+proxy = {"https": "127.0.0.1.8000"}
+os.system("clear")
+print(Fore.GREEN)
+print("""                                                                                                                                      _____                    
+ _ __ ___   __ ___  _| |_ ___  _ __ 
+| '_ ` _ \ / _` \ \/ / __/ _ \| '__|
+| | | | | | (_| |>  <| || (_) | |   
+|_| |_| |_|\__,_/_/\_\\__\___/|_|
+
+""")
+sleep(2)
+os.system("clear")
+print(Fore.RED)
+print("""
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ _ __ ___   __ ___  _| |_ ___  _ __ 
+| '_ ` _ \ / _` \ \/ / __/ _ \| '__|
+| | | | | | (_| |>  <| || (_) | |   
+|_| |_| |_|\__,_/_/\_\\__\___/|_|
+                                                                   
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  
+  ------ >>>>>> t.me/arshia_4700
+			                                                                                                                    
+""")
+
+#!/usr/bin/python
+
+
+
+# id telegram => @arshia_4700
+# soali dashtin beporsin
+####______
+# importing Libraryes
+import os, time
+
+# gettiing time !
+ti = input(" enter the time > ")
+while True:
+	    # send command to system #background
+        os.system("killall -HUP tor")
+        # time wait
+        time.sleep(int(ti))
+        # im maxtor
+        # i love all 🖤
+        # script end.
+        
